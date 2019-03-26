5877d5c7ebb020860c5086952fe93dc31f7d70a3
ingridpacheco <iq.pacheco23@gmail.com>
2019-03-25 22:53:23 -0300

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
@@ -11,9 +12,10 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 import MenuItem from '@material-ui/core/MenuItem';
-import './App.css';
-import logo from './maxresdefault.jpg';
+import logo from './bg-index.jpg';
 import Calculate from './calculateLogic';
+import Divider from '@material-ui/core/Divider';
+import './App.css';
 
 export function parseExpression(exp){
   let result = ''
@@ -106,65 +108,70 @@ class App extends Component {
   render() {
     let variables = this.state.variables.slice(0,this.state.quantity)
     return (
-      <div className="App">
-        <img src={logo} height={200}/>
-        <h3>Trabalho de Lógica em Programação</h3>
-        <div style={{display: 'inline-grid'}}>
-          <ExpansionPanel defaultExpanded>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>Valor das Variáveis</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              {variables.map(item => (
-                <FormControl required variant="outlined" style={{width: 100}} key={item}>
-                  <InputLabel
-                    ref={ref => {
-                      this.InputLabelRef = ref;
-                    }}
-                    htmlFor="outlined-age-simple"
-                    style={{bottom: 50, top: 'auto'}}
-                  >
-                    Valor de {item}
-                  </InputLabel>
-                  <Select
-                    value={this.state.varValues[item]}
-                    onChange={this.changeVariable(item)}
-                    input={
-                      <OutlinedInput
-                        labelWidth={0}
-                        name="age"
-                        id="outlined-age-simple"
-                      />
-                    }
-                  >
-                    <MenuItem value={true}>True</MenuItem>
-                    <MenuItem value={false}>False</MenuItem>
-                  </Select>
-                </FormControl>
-              ))}
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <TextField
-              id="logic"
-              label="Lógica"
-              onChange={this.handleChange}
+      <div className="App" style={{backgroundColor: '#efeeee'}}>
+        <div className="crop">
+          <img src={logo} alt="logProg"/>
+        </div>
+        <Divider variant="middle" />
+        <h2>Trabalho de Lógica em Programação</h2>
+        <Paper elevation={2} style={{width: 400, marginLeft: '40%', paddingTop: 20, paddingBottom: 20}}>
+          <div style={{display: 'inline-grid'}}>
+            <ExpansionPanel defaultExpanded>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>Valor das Variáveis</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                {variables.map(item => (
+                  <FormControl required variant="outlined" style={{width: 100}} key={item}>
+                    <InputLabel
+                      ref={ref => {
+                        this.InputLabelRef = ref;
+                      }}
+                      htmlFor="outlined-age-simple"
+                      style={{bottom: 50, top: 'auto'}}
+                    >
+                      Valor de {item}
+                    </InputLabel>
+                    <Select
+                      value={this.state.varValues[item]}
+                      onChange={this.changeVariable(item)}
+                      input={
+                        <OutlinedInput
+                          labelWidth={0}
+                          name="age"
+                          id="outlined-age-simple"
+                        />
+                      }
+                    >
+                      <MenuItem value={true}>True</MenuItem>
+                      <MenuItem value={false}>False</MenuItem>
+                    </Select>
+                  </FormControl>
+                ))}
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <TextField
+                id="logic"
+                label="Lógica"
+                onChange={this.handleChange}
+                margin="normal"
+                variant="outlined"
+                style={{marginRight: 50}}
+            />
+            <Button variant="contained" color="default" style={{marginTop: 25}} onClick={this.getResult}>
+              Enviar
+            </Button>
+            <TextField
+              error
+              disabled
+              id="result"
+              label="Resultado"
+              value={this.state.result}
               margin="normal"
               variant="outlined"
-              style={{marginRight: 50}}
-          />
-          <Button variant="contained" color="default" style={{marginTop: 25}} onClick={this.getResult}>
-            Enviar
-          </Button>
-          <TextField
-            error
-            disabled
-            id="result"
-            label="Resultado"
-            value={this.state.result}
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
+            />
+          </div>
+        </Paper>
       </div>
     );
   }
