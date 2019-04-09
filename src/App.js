53b833c664e1ce116a8a184e2aae4b73bb226cb3
ingridpacheco <iq.pacheco23@gmail.com>
2019-04-09 18:17:33 -0300

    Fix input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
 import logo from './bg-index.jpg';
+import grafo from './graph.png';
 import Calculate from './calculateLogic';
-import Divider from '@material-ui/core/Divider';
+import { 
+  Divider,
+  FormControl,
+  Button,
+  TextField,
+  Paper,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormGroup,
+  Checkbox
+} from '@material-ui/core';
 import './App.css';
 
 class Graph {
@@ -35,6 +45,11 @@ class Graph {
     this.LetterList.get(v).push(letter)
   }
 
+  removeLetter(v, letter){
+    let index = this.LetterList.get(v).indexOf(letter)
+    this.LetterList.get(v).splice(index,1)
+  }
+
   getAdjacents(v){
     return this.AdjList.get(v)
   }
@@ -42,6 +57,26 @@ class Graph {
   letterinList(v,letter){
     return this.LetterList.get(v).includes(letter)
   }
+
+  printGraph(){ 
+      var get_keys = this.AdjList.keys(); 
+    
+      for (var i of get_keys)  
+      { 
+          var get_values = this.AdjList.get(i);
+          var get_letters = this.LetterList.get(i);
+          var conc = "";
+          var letters = "";
+    
+          for (var j of get_values)
+            conc += j + " ";
+            
+          for (var k of get_letters)
+            letters += k + " ";
+    
+          console.log(i + " -> " + conc + " -> " + letters); 
+      } 
+  } 
 }
 
 export function parseExpression(exp){
@@ -73,21 +108,27 @@ class App extends Component {
     this.state ={
       logic: "",
       result: "",
-      quantity: 1,
-      varValues: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-        F: false,
-        G: false,
-        H: false,
-        I: false,
-        J: false
-      },
       variables: [],
+      rootNode: 's1',
+      vertices: ['s1','s2','s3','s4','s5'],
+      graph: new Graph()
     }
+  }
+
+  componentDidMount(){
+    let {graph} = this.state
+    graph.addVertex('s1')
+    graph.addVertex('s2')
+    graph.addVertex('s3')
+    graph.addVertex('s4')
+    graph.addVertex('s5')
+    graph.addEdge('s1', 's2'); 
+    graph.addEdge('s1', 's3'); 
+    graph.addEdge('s2', 's4');
+    graph.addEdge('s3', 's5');
+    graph.addEdge('s4', 's5');
+    graph.setRootNode('s1')
+    this.setState({graph})
   }
 
   handleChange = event => {
@@ -97,21 +138,9 @@ class App extends Component {
     });
   };
 
-  changeQuantity = event => {
-    this.setState({
-      quantity: event.target.value
-    })
-  }
-
-  changeVariable = name => event => {
-    let valuesCopy = Object.assign({}, this.state.varValues)
-    valuesCopy[name] = event.target.value
-    this.setState({varValues: valuesCopy})
-  }
-
   getResult = () => {
     let logic = this.state.logic.replace(/ /g,'').toUpperCase()
-    let quantity = 0
+    let {graph} = this.state
     let result = []
     let variables = []
 
@@ -120,39 +149,37 @@ class App extends Component {
     for (let i = 0; i < logic.length; i++){
       if (!['V','^','➡','~','<','>','[',']'].includes(logic[i]) && !variables.includes(logic[i])){
         variables.push(logic[i])
-        quantity++;
       }
       result.push(logic[i])
     }
 
-    var graph = new Graph(4); 
-    var vertices = [ 's1', 's2', 's3', 's4']; 
-      
-    // adding vertices 
-    for (var i = 0; i < vertices.length; i++) { 
-        graph.addVertex(vertices[i]); 
-    }
-
-    graph.setRootNode('s1')
-      
-    // adding edges 
-    graph.addEdge('s1', 's2'); 
-    graph.addEdge('s1', 's3'); 
-    graph.addEdge('s2', 's4');
-    graph.addLetter('s1', 'A'); 
-    graph.addLetter('s2', 'B'); 
-    graph.addLetter('s3', 'A');
-    graph.addLetter('s3', 'B');
-    graph.addLetter('s4', 'A');
-
     this.setState({
-      quantity,
       result: Calculate(result, graph),
       variables
     });
   }
 
+  changeRootNode = event => {
+    let rootNode = event.target.value
+    let {graph} = this.state
+    graph.setRootNode(rootNode)
+    this.setState({
+      rootNode,
+      graph
+    })
+  }
+
+  changeLetters = (letter, vertice) => event => {
+    let {graph} = this.state
+    !graph.letterinList(vertice, letter) ? graph.addLetter(vertice, letter) :
+      graph.removeLetter(vertice, letter)
+    this.setState({
+      graph
+    })
+  }
+
   render() {
+    let {vertices, variables, graph} = this.state
     return (
       <div className="App" style={{backgroundColor: '#efeeee'}}>
         <div className="crop">
@@ -162,22 +189,65 @@ class App extends Component {
         <h2>Trabalho de Lógica em Programação</h2>
         <Paper elevation={2} style={{paddingTop: 20, paddingBottom: 20}}>
           <div style={{display: 'inline-grid'}}>
+              <img src={grafo} alt="grafo" style={{width: 380}}/>
+              <FormControl component="fieldset">
+                <FormLabel component="legend" style={{textAlign: 'left', fontSize: 14, paddingLeft: 15}}>Nó Raiz</FormLabel>
+                <RadioGroup
+                  aria-label="rootNode"
+                  name="rootNode"
+                  style={{paddingLeft: 20}}
+                  value={this.state.rootNode}
+                  onChange={this.changeRootNode}
+                  row
+                >
+                  {vertices.map(item => (
+                    <FormControlLabel
+                      key={item}
+                      value={item}
+                      control={<Radio color="primary"/>}
+                      label={item}
+                      labelPlacement="end"
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              {variables.map(item => (
+                <Paper key={item} elevation={2} style={{paddingTop: 20, paddingBottom: 20}}>
+                  <FormLabel component="legend" style={{textAlign: 'left', fontSize: 14, paddingLeft: 15}}>Vértices que tem {item}:</FormLabel>
+                  <FormGroup row style={{paddingLeft: 15}}>
+                    {vertices.map(vertice => (
+                      <FormControlLabel
+                        key={vertice}
+                        control={
+                          <Checkbox
+                            checked={graph.letterinList(vertice, item)}
+                            onChange={this.changeLetters(item, vertice)}
+                            value={vertice}
+                          />
+                        }
+                        label={vertice}
+                      />
+                    ))}
+                  </FormGroup>
+                </Paper>
+            ))}
             <TextField
               id="logic"
               label="Lógica"
               onChange={this.handleChange}
               margin="normal"
               variant="outlined"
-              style={{marginRight: 50}}
+              style={{marginRight: 50, marginLeft: 10}}
             />
-            <Button variant="contained" color="default" style={{marginTop: 25}} onClick={this.getResult}>
-              Enviar
+            <Button variant="contained" color="default" style={{marginTop: 25, maxWidth: 380}} onClick={this.getResult}>
+              VERIFICAR RESULTADO
             </Button>
             <TextField
               error
               disabled
               id="result"
               label="Resultado"
+              style={{width: 320, marginLeft: 10}}
               value={this.state.result}
               margin="normal"
               variant="outlined"
