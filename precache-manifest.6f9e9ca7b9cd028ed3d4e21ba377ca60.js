ed3c6cc32c44773ba13775de212c1a55fe809128
ingridpacheco <iq.pacheco23@gmail.com>
2019-04-09 18:33:58 -0300

    Updates

---
+++ precache-manifest.6f9e9ca7b9cd028ed3d4e21ba377ca60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "08d827ce98308f168f47",
+    "url": "/trabalho-resolver-logica/static/css/main.e2dea9cb.chunk.css"
+  },
+  {
+    "revision": "08d827ce98308f168f47",
+    "url": "/trabalho-resolver-logica/static/js/main.8bacb513.chunk.js"
+  },
+  {
+    "revision": "3ee08b64c9d7731cf168",
+    "url": "/trabalho-resolver-logica/static/js/runtime~main.746a7b87.js"
+  },
+  {
+    "revision": "086019f7b75c1b91ae35",
+    "url": "/trabalho-resolver-logica/static/js/2.5cbc308e.chunk.js"
+  },
+  {
+    "revision": "be70d7dd4747afad93cef1636ef707c0",
+    "url": "/trabalho-resolver-logica/static/media/bg-index.be70d7dd.jpg"
+  },
+  {
+    "revision": "fa0923e56fc1e7365669571836853cdb",
+    "url": "/trabalho-resolver-logica/static/media/graph.fa0923e5.png"
+  },
+  {
+    "revision": "d74a4cd42dbc5b1e577d9922d287e7fd",
+    "url": "/trabalho-resolver-logica/index.html"
+  }
+];
